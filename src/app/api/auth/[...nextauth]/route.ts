00daebea75afd0cbfe44647840883cc256a5bfdf
gemasaputera <gema.saputera@gmail.com>
2024-08-13 11:12:25 +0700

    feat: update code formatting settings and import statements

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,2 @@
-export { GET, POST } from '../../../../auth';
-export const runtime = 'edge';
+import { handlers } from "@/auth" // Referring to the auth.ts we just created
+export const { GET, POST } = handlers
--- src/auth.ts
+++ src/auth.ts
@@ -1,22 +1,15 @@
-import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 import Google from 'next-auth/providers/google';
-
-export const {
-  handlers: { GET, POST },
-  auth
-} = NextAuth({
+import NextAuth from "next-auth"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GitHub({
       clientId: process.env.OAUTH_CLIENT_KEY as string,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET as string
+      clientSecret: process.env.OAUTH_CLIENT_SECRET as string,
     }),
     Google({
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-    })
-  ],
-  pages: {
-    signIn: '/sign-in'
-  }
-});
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),],
+})
