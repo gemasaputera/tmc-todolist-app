371bbb7c2916eb475d76eadf30926baf26562720
gemasaputera <gema.saputera@gmail.com>
2025-05-28 14:19:58 +0700

    refactor(loading): replace custom spinner with Mantine Loader component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,11 @@
+import { Container, Loader } from '@mantine/core';
 import React from 'react';
-import { HiArrowPath } from 'react-icons/hi2';
 
 const LoadingPage = () => {
   return (
-    <div className='flex justify-center py-8'>
-      <HiArrowPath className='w-8 h-8 text-orange-700 animate-spin' />
-    </div>
+    <Container>
+      <Loader color='blue' />
+    </Container>
   );
 };
 
